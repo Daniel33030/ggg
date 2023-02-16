94e44d6f150c15b7e2b3fb1a4246e36b4be7196e
Daniel33030 <122061718+Daniel33030@users.noreply.github.com>
2023-02-16 18:58:40 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var background
+
+
+
+function preload(){
+loadImage.background = "Forest_Background_Vector_9.jpg"
+
+}
+
+function setup() {
+    background.velocityX = -2
+}
+
+function draw() {
+ background()
+}
